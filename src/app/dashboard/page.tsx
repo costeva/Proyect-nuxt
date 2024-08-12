f92944e46d3a69060a4588285af6aa24a3af71d4
Alexis Costedoat <67351701+costeva@users.noreply.github.com>
2024-08-12 12:36:57 -0300

    Arreglos de grid, armado de search.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,13 @@
 import { lusitana } from "../ui/fonts";
 import RevenueChart from "../ui/dashboard/revenue-chart";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import Card from "../ui/dashboard/cards";
 import { Suspense } from "react";
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "../ui/skeletons";
 export default function Page() {
   return (
     <main>
@@ -8,21 +15,17 @@ export default function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        {/* <Card title="Collected" value={totalPaidInvoices} type="collected" /> */}
-        {/* <Card title="Pending" value={totalPendingInvoices} type="pending" /> */}
-        {/* <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> */}
-        {/* <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> */}
+        <Suspense fallback={<CardsSkeleton />}>
+          <Card />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          {<LatestInvoices />}
+        </Suspense>
       </div>
     </main>
   );
